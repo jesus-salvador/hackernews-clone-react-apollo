d4af1247cd8c10f0a8ace90d15259772ad28d8c1
Jesus Meza <jsmsmail@gmail.com>
2023-04-24 17:02:41 -0600

    adding rounting

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,30 @@ import React from 'react'
 import logo from './../logo.svg';
 import './../styles/App.css';
 import CreateLink from './CreateLink';
+import Header from  './Header'
 import LinkList from './LinkList';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <CreateLink />
-        <LinkList />
+        <div className='w85'>
+          <Header />
+        </div>
+        <div className='ph3 pv1 white-gray'>
+          <Routes>
+            <Route
+              path='/'
+              element={<LinkList/>}
+            />
+            <Route
+              path='/create'
+              element={<CreateLink/>}
+            />
+          </Routes>
+        </div>
       </header>
       <footer>
         <a
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
@@ -22,9 +23,11 @@ const client = new ApolloClient({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
